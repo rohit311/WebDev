96c469c377065b68a97a7e27a975cb2eb07bb4e5
rohit369 <scorpiorohit311@gmail.com>
2025-02-10 23:27:16 +0530

    Update ConversionService.java

--- converter/src/main/java/com/example/restapi/service/ConversionService.java
+++ converter/src/main/java/com/example/restapi/service/ConversionService.java
@@ -1,6 +1,8 @@
 package com.example.restapi.service;
 
 import com.example.restapi.model.Length;
+import com.example.restapi.model.Temperature;
+import com.example.restapi.model.Weight;
 
 public class ConversionService {
 
@@ -8,6 +10,13 @@ public class ConversionService {
     if (type.equalsIgnoreCase("length")) {
       return new Length(fromUnit, value).getValue(toUnit);
     }
+    else if (type.equalsIgnoreCase("weight")) {
+      return new Weight(fromUnit, value).getValue(toUnit);
+    }
+    else if (type.equalsIgnoreCase("temperature")) {
+      return new Temperature(fromUnit, value).getValue(toUnit);
+    }
+
 
     return 0.0;
   }
